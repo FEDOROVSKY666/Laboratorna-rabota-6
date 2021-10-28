04d8eb60adecb1ffc514d20b7c7492d7d0da5669
Alexandr <Fedorovsky666@gmail.com>
2021-10-29 00:09:43 +0300

    first

---
+++ LR7.cpp
@@ -0,0 +1,19 @@
+﻿
+
+#include <iostream>
+#include <iomanip> 
+#include "windows.h"
+using namespace std;
+int main()
+{
+	float y, x;
+    cout << "input x:";
+	cin >> x;
+	y = 0;
+	for (int i = 1; i < 4; i++) {
+		y += i * x;
+}
+	y = pow(x, 3) + sin(y);
+	cout << "Y=" << setprecision(3) << y << endl;
+}
+
