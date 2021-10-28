4ac2830fe8d6c8182f959f717d78a638c08bab54
Alexandr <Fedorovsky666@gmail.com>
2021-10-29 00:18:27 +0300

    first

---
+++ LR7(2).cpp
@@ -0,0 +1,16 @@
+﻿#include<iostream>
+#include <iomanip> 
+#include "windows.h"
+using namespace std;
+int main()
+{
+	float Z, y;
+	cout << "input y:";
+	cin >> y;
+	Z = 1;
+	for (int n = 1; n <= 10; n++) {
+		Z = Z*n;
+	}
+	Z = (y + Z + 2) / (y + 2);
+	cout << "Z=" << setprecision(3) << Z << endl;
+}
