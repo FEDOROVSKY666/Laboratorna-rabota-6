9d2472ede2d19f5b9f30f592eb9077e6258f0452
Alexandr <Fedorovsky666@gmail.com>
2021-11-18 23:20:07 +0200

    new SWITCH.cpp

---
+++ SWITCH.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include "windows.h"
+
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	char k;
+	cout << "Введіть пунктуаційний знак\n";
+	cin >> k;
+
+	switch (k)
+	{
+	case ',': cout << "Кома"; break;
+	case ':': cout << "Двокрапка"; break;
+	case '.': cout << "Крапка"; break;
+	case '-': cout << "Дефіс"; break;
+	case '?': cout << "Знак питання"; break;
+	case '!': cout << "Знак оклику"; break;
+	case ';': cout << "Крапка з комою"; break;
+
+	default: cout << "Не існує"; break;
+	}
+
+}
